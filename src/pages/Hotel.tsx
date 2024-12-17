8c294e8bf8a03d5735d1f40d15c30dbba549c3b6
taeyun <lwe12f675@gmail.com>
2024-12-17 15:13:26 +0900

    feat: 호텔 상세 페이지 구현 - 객실 리스트와 실시간 room 잔여수 (firebase의 실시간성을 이용해 매진임박 및 매진 표시)

--- src/pages/Hotel.tsx
+++ src/pages/Hotel.tsx
@@ -1,6 +1,7 @@
 import Carousel from '@/components/hotel/Carousel'
 import Contents from '@/components/hotel/Contents'
 import useHotel from '@/components/hotel/hooks/useHotel'
+import Rooms from '@/components/hotel/Rooms'
 import Top from '@/components/shared/Top'
 import { useParams } from 'react-router-dom'
 
@@ -22,6 +23,7 @@ const HotelPage = () => {
     <div>
       <Top title={name} subtitle={comment} />
       <Carousel images={images} />
+      <Rooms hotelId={id} />
       <Contents contents={contents} />
     </div>
   )
