a9158f5ff85f0c9c0700f562b2fa2c0175962720
taeyun <lwe12f675@gmail.com>
2024-12-19 15:15:35 +0900

    feat: 소셜 로그인 - 구글 로그인 후 유저정보 firebase에 저장

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import HotelList from '@pages/HotelList'
 import Test from '@pages/Test'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import useLoadKakao from '@/hooks/useLoadKakao'
+import SigninPage from '@/pages/Signin'
+import MyPage from '@/pages/My'
 
 function App() {
   useLoadKakao()
@@ -12,6 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<HotelList />} />
           <Route path="/hotel/:id" element={<HotelPage />} />
+          <Route path="/my" element={<MyPage />} />
+          <Route path="/signin" element={<SigninPage />} />
           <Route path="/test" element={<Test />} />
         </Routes>
       </BrowserRouter>
